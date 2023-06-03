f86f7d81140c761c9a6ec1b11859bda643281e83
PeterK-UWP <kveto002@rangers.uwp.edu>
2023-06-02 20:59:42 -0500

    wag tail power series

--- power_series/testing.py
+++ power_series/testing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # input 4 [a0 a1 a2 a3] lambda shift_xi
 def recursion_relationship(recursion_index, recursion_coefficients, shifted_energy, potential_shift):
     """
@@ -15,7 +16,7 @@ def recursion_relationship(recursion_index, recursion_coefficients, shifted_ener
                       ((recursion_index - 1) * (recursion_index))
 
     # new_coefficient = -recursion_coefficients[2]/(recursion_index+2)
-    return new_coefficient # a4
+    return new_coefficient  # a4
 
 
 def wave_function(reduced_position, maximum_coefficient, starting_coefficients, simulation_parameters):
@@ -51,54 +52,57 @@ if __name__ == '__main__':
     shift_xi = 1
     # parkers epsilon: 0.3094688	0.73422037	1.49997346	2.19740304	2.99866356
 
-    epsilon = 4.609 # 1.500 n=1 shift=1 2.998601 # 6.27841
-    # 0.309459 first state (bell curve blown up at the end) note due to no trunkation, positive xi blows up exponentially
+    #epsilon = 1.6  # 1.500 n=1 shift=1 2.998601 # 6.27841
+    #for epsilon in np.linspace(0, 1.5, 15):
+    for epsilon in np.linspace(4.3, 4.4, 10):
+        # 0.309459 first state (bell curve blown up at the end) note due to no trunkation, positive xi blows up exponentially
 
-    # 1.500000 second state (end behavior again messed up dueto no trunkation)
+        # 1.500000 second state (end behavior again messed up dueto no trunkation)
 
-    eigenvalue = epsilon - shift_xi**2/2
-    """
-    Here we wish to loop over many epsilons to find multiply eigenvalues via wag the dog
-    
-    for epsilon in range(0, 4, 1):
         eigenvalue = epsilon - shift_xi ** 2 / 2
+        """
+        Here we wish to loop over many epsilons to find multiply eigenvalues via wag the dog
 
-        reduced_positions = np.linspace(-10, 10, num=1_000)
-        maximum_index = 10_000
+        for epsilon in range(0, 4, 1):
+            eigenvalue = epsilon - shift_xi ** 2 / 2
+
+            reduced_positions = np.linspace(-10, 10, num=1_000)
+            maximum_index = 10_000
+            initial_coefficients = [a_0, a_1]
+
+            psi_values = wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])
+            recursion_relationship(4, initial_coefficients, eigenvalue, shift_xi)
+            plt.plot(reduced_positions, psi_values)
+            plt.xlim([-10, 10])
+            plt.ylim([-3.5, 3.5])
+        plt.show()
+    """
+
+        #print(f'a_2 = {indicial_equations(eigenvalue, [a_0, a_1])[0]},'
+        #      f'a_3 = {indicial_equations(eigenvalue, [a_0, a_1])[1]}')
+        # print(f' a_terms = {recursion_relation(indicial_equations(eigenvalue), eigenvalue)}')
+        # print(plot_expansion(recursion_relation(indicial_equations(eigenvalue), eigenvalue)))
         initial_coefficients = [a_0, a_1]
+        for coefficient in indicial_equations(eigenvalue, initial_coefficients):
+            initial_coefficients.append(coefficient)
+        #print(initial_coefficients)
+        #print(recursion_relationship(4, initial_coefficients, eigenvalue, shift_xi))  # I agree
+
+        reduced_positions = np.linspace(-10, 10, num=10_001)
+        maximum_index = 10_000
+        #
+        # print(len(reduced_positions)) = 1000
+        # print(len(wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])[1])) = 10,001
 
         psi_values = wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])
-        recursion_relationship(4, initial_coefficients, eigenvalue, shift_xi)
+        #
         plt.plot(reduced_positions, psi_values)
         plt.xlim([-10, 10])
         plt.ylim([-3.5, 3.5])
+        plt.axhline()
+        plt.axvline(1)
+        # plt.savefig("eigenvalue_1")
     plt.show()
-"""
+        # print(epsilon)
 
-    print(f'a_2 = {indicial_equations(eigenvalue, [a_0, a_1])[0]},'
-          f'a_3 = {indicial_equations(eigenvalue, [a_0, a_1])[1]}')
-    # print(f' a_terms = {recursion_relation(indicial_equations(eigenvalue), eigenvalue)}')
-    #print(plot_expansion(recursion_relation(indicial_equations(eigenvalue), eigenvalue)))
-    initial_coefficients = [a_0, a_1]
-    for coefficient in indicial_equations(eigenvalue, initial_coefficients):
-        initial_coefficients.append(coefficient)
-    print(initial_coefficients)
-    print(recursion_relationship(4, initial_coefficients, eigenvalue, shift_xi)) # I agree
-
-    reduced_positions = np.linspace(-10, 10, num=10_001)
-    maximum_index = 10_000
-    #
-    #print(len(reduced_positions)) = 1000
-    #print(len(wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])[1])) = 10,001
-
-    psi_values = wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])
-    #
-    plt.plot(reduced_positions, psi_values)
-    plt.xlim([-10, 10])
-    plt.ylim([-3.5, 3.5])
-    plt.axhline()
-    plt.axvline(1)
-    plt.savefig("eigenvalue_5")
-    plt.show()
-    # print(epsilon)
 
