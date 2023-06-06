7fc1069f5d6cb64c138cdd9a7cec327f1d39f342
williamdparker <parkerw@uwp.edu>
2023-06-06 08:23:07 -0500

    Added dynamic plotting of wag the dog for testing.py

--- power_series/testing.py
+++ power_series/testing.py
@@ -13,7 +13,7 @@ def recursion_relationship(recursion_index, recursion_coefficients, shifted_ener
     """
     new_coefficient = (- 2 * shifted_energy * recursion_coefficients[2] + recursion_coefficients[0] +
                        2 * potential_shift * recursion_coefficients[1]) / \
-                      ((recursion_index - 1) * (recursion_index))
+                      ((recursion_index - 1) * recursion_index)
 
     # new_coefficient = -recursion_coefficients[2]/(recursion_index+2)
     return new_coefficient  # a4
@@ -36,6 +36,8 @@ def wave_function(reduced_position, maximum_coefficient, starting_coefficients, 
 
 def indicial_equations(eigenvalue, coefficients):
     a_2 = -eigenvalue * coefficients[0]
+
+    # The series should also depend on the sign of xi as well as the sign of xi0
     if shift_xi >= 0:
         a_3 = -(eigenvalue * coefficients[1] - shift_xi * coefficients[0]) / 3
     if shift_xi < 0:
@@ -51,10 +53,14 @@ if __name__ == '__main__':
     a_1 = 0
     shift_xi = -1
     # parkers epsilon: 0.3094688	0.73422037	1.49997346	2.19740304	2.99866356
-
+    test_epsilon = 4
+    delta = 1e-0
+    test_number = 20
+    test_epsilons = np.linspace(test_epsilon - delta, test_epsilon + delta, test_number)
     #epsilon = 1.6  # 1.500 n=1 shift=1 2.998601 # 6.27841
     #for epsilon in np.linspace(0, 1.5, 15):
-    for epsilon in np.linspace(2.5, 3.5, 10):  # +inf end behavior blows up to +inf so the -1 shift is harder to get
+    #for epsilon in np.linspace(2.5, 3.5, 10):  # +inf end behavior blows up to +inf so the -1 shift is harder to get
+    for epsilon in test_epsilons:
         eigenvalue = epsilon - shift_xi ** 2 / 2
         """
         Here we wish to loop over many epsilons to find multiply eigenvalues via wag the dog
@@ -80,13 +86,22 @@ if __name__ == '__main__':
 
         psi_values = wave_function(reduced_positions, maximum_index, initial_coefficients, [eigenvalue, shift_xi])
         #
-        plt.plot(reduced_positions, psi_values)
-        plt.xlim([-10, 10])
-        plt.ylim([-3.5, 3.5])
-        plt.axhline()
-        plt.axvline(1)
-        # plt.savefig("eigenvalue_1")
+        plt.plot(reduced_positions, psi_values, label=f'{epsilon:.5f}')
+    plt.xlim([-10, 10])
+    plt.ylim([-10, 10])
+    plt.axhline()
+    plt.axvline(1)
+    plt.legend(title=r'$\epsilon$')
+    plot_ranges = [np.diff(plt.xlim()), np.diff(plt.ylim())]
+    plot_minima = [plt.xlim()[0], plt.ylim()[0]]
+    shift_label_position = [plot_minima[0] + 0.1*plot_ranges[0],
+                            plot_minima[1] + 0.9*plot_ranges[1]]
+    plt.text(shift_label_position[0], shift_label_position[1], r'$\xi_0$ = ' + f'{shift_xi}',
+             ha='center')
+    # plt.savefig("eigenvalue_1")
+    # plt.savefig("DSOWaveFunction-1PowerSeriesn0.png")
+
     plt.show()
-        # print(epsilon)
+    # print(epsilon)
 
 
